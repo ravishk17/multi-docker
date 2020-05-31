e2fa5665dc2084db0d9ec8de899c385ae0c30429
Ravish Kumar <ravish.k17@iiits.in>
2020-06-01 00:27:02 +0530

    latest

--- backend/routes/feed.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const feedController = require("../controllers/feed");
-const router = express.Router();
-
-router.post("/book",feedController.book);
-
-module.exports = router;
